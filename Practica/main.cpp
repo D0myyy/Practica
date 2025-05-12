6e8c06a00040b2f06c53bc7013e207acb1c4996f
D0myyy <tdumitruyt@gmail.com>
2025-05-12 13:46:56 +0300

    day 1 finale

--- Practica/main.cpp
+++ Practica/main.cpp
@@ -2,8 +2,13 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
+
+void addToy(string name, int price, int quantity, string category, int ID){
+
+}
 
 int main(){
     ifstream Toys("Toys.txt");
@@ -22,16 +27,26 @@ int main(){
         string name, category;
         int price, quantity, ID;
         iss >> name >> price >> quantity >> category >> ID;
-        cout << name << " " << price << " " << quantity << " " << category << " " << ID << endl;
+        //cout << name << " " << price << " " << quantity << " " << category << " " << ID << endl;
+        cout << left << setw(15) << name
+             << setw(10) << price
+             << setw(10) << quantity
+             << setw(15) << category
+             << setw(5) << ID << endl;
     }
+    cout << endl;
 
     while(getline(Manufacturer, lineManufacturer)){
         istringstream iss(lineManufacturer);
-        string name, category;
-        int price, quantity, ID;
-        iss >> name >> price >> quantity >> category >> ID;
-        cout << name << " " << price << " " << quantity << " " << category << " " << ID << endl;
+        string name, country;
+        int ID;
+        iss >> ID >> name >> country;
+        //cout << ID << " " << name << " " << country << endl;
+        cout << left << setw(5) << ID
+             << setw(15) << name
+             << setw(15) << country << endl;
     }
+    cout << endl;
 
     Toys.close();
     Manufacturer.close();
